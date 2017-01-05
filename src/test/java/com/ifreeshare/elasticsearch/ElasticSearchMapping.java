20a394a065b568b71c49e5614027ed6c9ffcd456
714303584@qq.com <714303584@qq.com>
2017-01-05 14:47:53 +0800

    ElasticSearch Mapping
    url
    	----- image
    	----- html

---
+++ src/test/java/com/ifreeshare/elasticsearch/ElasticSearchMapping.java
@@ -0,0 +1,159 @@
+package com.ifreeshare.elasticsearch;
+
+import io.vertx.core.json.JsonObject;
+
+public class ElasticSearchMapping {
+	
+	public static final String keyword = "keyword";
+	
+	public static final String text = "text";
+	
+	public static final String Longs = "long";
+	
+//	public static final String 
+	
+	
+	public static void main(String[] args) {
+//		
+//		long current = System.currentTimeMillis();
+//		
+//		User user = new User();
+//		
+//		user.setName("zhushunshan");
+//		user.setPassword("zhushunshan1234567");
+//		for (long i = 0; i < 1000000L; i++) {
+//			user.setName("zhushunshan"+i);
+//			String name = user.getName();
+//		}
+//		
+//		System.err.println(System.currentTimeMillis()  - current);
+//		
+//		
+//		
+//		current = System.currentTimeMillis();
+//		
+//		JsonObject jsonUser = new JsonObject().put(name, "zhushunshan").put(password, "zhushunshan1234567");
+//		
+//		for (long i = 0; i < 1000000L; i++) {
+//			jsonUser.put(name,"zhushunshan"+i);
+//			String name2 = jsonUser.getString(name);
+//		}
+//		
+//		System.err.println(System.currentTimeMillis()  - current);
+		
+		getHTMLMapping();
+		
+	}
+	
+	
+	public static void getHTMLMapping(){
+		
+		JsonObject imageMapJson = new JsonObject();
+		
+		JsonObject _all = new JsonObject();
+		_all.put("analyzer", "ik_max_word");
+		_all.put("search_analyzer", "ik_max_word");
+		
+		JsonObject properties = new JsonObject();
+		
+		imageMapJson.put("_all", _all);
+		
+		imageMapJson.put("properties", properties);
+		
+		properties.put("charset", getField("charset", keyword, true));
+		
+		properties.put("Content-Type", getField("Content-Type", keyword, true));
+		
+		properties.put("create_date", getField("create_date", Longs, true));
+		
+		properties.put("keywords", getField("keywords", text, true).put("analyzer", "ik_max_word").put("search_analyzer", "ik_max_word"));
+		
+		System.out.println(imageMapJson.encodePrettily());
+		
+		
+		
+	}
+	
+	
+	
+	
+
+public static void getImageMapping(){
+	
+	JsonObject imageMapJson = new JsonObject();
+	
+	JsonObject _all = new JsonObject();
+	_all.put("analyzer", "ik_max_word");
+	_all.put("search_analyzer", "ik_max_word");
+	
+	JsonObject properties = new JsonObject();
+	
+	imageMapJson.put("_all", _all);
+	
+	imageMapJson.put("properties", properties);
+	
+	properties.put("thumbnail", getField("thumbnail", keyword, true));
+	properties.put("resolution", getField("resolution", keyword, true));
+	properties.put("uuid", getField("uuid", keyword, true));
+	
+	
+	properties.put("sha1", getField("sha1", keyword, true));
+	properties.put("md5", getField("md5", keyword, true));
+	properties.put("Content-Type", getField("Content-Type", keyword, true));
+	
+	properties.put("file_url_path", getField("file_url_path", keyword, true));
+	properties.put("url", getField("url", keyword, true));
+	properties.put("filename", getField("filename", keyword, true));
+	properties.put("fileSize", getField("fileSize", Longs, true));
+	properties.put("create_date", getField("create_date", Longs, true));
+	
+	properties.put("keywords", getField("keywords", text, true).put("analyzer", "ik_max_word").put("search_analyzer", "ik_max_word"));
+	
+	System.out.println(imageMapJson.encodePrettily());
+	
+	
+	
+}
+
+public static JsonObject getField(String name, String type, boolean index){
+	JsonObject fieldInfo = new JsonObject();
+	fieldInfo.put("type", type);
+	fieldInfo.put("index", index);
+	return fieldInfo;
+	
+}
+
+
+	
+
+}
+
+
+
+
+
+ class User{
+	
+	private String name;
+	
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
